ada2220763042ccfc33620b5f05d7d94f41305de
Rudrabose <92892605+Rudrabose@users.noreply.github.com>
2025-05-06 12:18:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -173,13 +173,6 @@ async def youtube_to_txt(client, message: Message):
         'cookies': 'youtube_cookies.txt'  # Specify the cookies file
     }
 
-    url = normalize_youtube_url(url)
-    def normalize_youtube_url(url):
-    if "youtube.com/embed/" in url:
-        video_id = url.split("/")[-1]
-        return f"https://www.youtube.com/watch?v={video_id}"
-    return url
-
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         try:
             result = ydl.extract_info(youtube_link, download=False)
